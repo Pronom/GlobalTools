be118ec50c59b052b4b0cc0f89830858549ae5a3
Storulv <jordycorvisiercontact@gmail.com>
2020-01-29 16:13:40 +0100

    16 : Calendar bot online ( regenerate token )

---
+++ calendar_bot/index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js'); //Ce que le bot à besoin /
+const client = new Discord.Client(); //Que votre Bot est un nouvel utilisateur
+var prefix = "*"; //Prefix de votre Bot ( *play www.youtube.com/ )
+client.login("NjY2NzEwMTI3Njg4Mjg2MjE4.XjGgLQ.af2_UJ83m1TmxFHk6K5C6lNNXEI"); //Token (Série de chiffre) propre a chaque Bot
+client.on("ready", () => { //Signifie que le bot à bien démarré 
+    console.log("Je suis prêt !"); //Lorsque que le bot est lancé observer la console Visual Studio 
+    client.user.setGame("Préparer le raid !"); //Voir le Jeu sur le Discord
+});
