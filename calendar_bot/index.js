deacdd072ebd4c27dc9246a4be9fe6e3fe913de8
Storulv <jordycorvisiercontact@gmail.com>
2020-01-29 16:34:46 +0100

    16 : HotFix ( new token )

--- calendar_bot/index.js
+++ calendar_bot/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js'); //Ce que le bot à besoin /
 const client = new Discord.Client(); //Que votre Bot est un nouvel utilisateur
 var prefix = "*"; //Prefix de votre Bot ( *play www.youtube.com/ )
-client.login(""); //Token (Série de chiffre) propre a chaque Bot
+client.login("NjY2NzEwMTI3Njg4Mjg2MjE4.XjGlZg.H2A1gkMreuFxJyldweekLvaYt0U"); //Token (Série de chiffre) propre a chaque Bot
 client.on("ready", () => { //Signifie que le bot à bien démarré 
     console.log("Je suis prêt !"); //Lorsque que le bot est lancé observer la console Visual Studio 
     client.user.setGame("Préparer le raid !"); //Voir le Jeu sur le Discord
